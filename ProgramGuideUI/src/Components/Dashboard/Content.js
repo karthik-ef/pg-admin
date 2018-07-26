cbecac22d9ccba0deca5e011eb21223ca8a88e57
Karthik Subbarayappa <karthik.subbarayappa@ef.com>
2018-07-26 13:33:40 +0530

    Navbar design issue fix

--- ProgramGuideUI/src/Components/Dashboard/Content.js
+++ ProgramGuideUI/src/Components/Dashboard/Content.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import SearchResults from '../SearchResult/SearchResults';
+import ComparePages from '../ComparePages';
+import AddUser from '../AddUser';
+import BulkUpload from '../BulkUpload';
 
 class Content extends Component {
   render() {
 
+    const component = window.location.pathname === '/SearchResults' ? <SearchResults />
+      : window.location.pathname === '/ComparePages' ? <ComparePages />
+        : window.location.pathname === '/AddUser' ? <AddUser />
+          : window.location.pathname === '/BulkUpload' ? <BulkUpload />
+            : '';
     return (
-        <div className="contentDiv">
-       <SearchResults/>
-    </div>
+
+      <div className="contentDiv">
+        {component}
+      </div>
     );
   }
 }
--- ProgramGuideUI/src/Components/Dashboard/Header.js
+++ ProgramGuideUI/src/Components/Dashboard/Header.js
@@ -1,49 +1,60 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import LogoutIcon from './Logout.png';
 
 class Header extends Component {
-  render() {
+    render() {
 
-    return (
-        <div className="headerDiv">
-        {/* Display Brand */}
-        <div className="brandDiv">
-            <img className="efLogo" src="//www.ef.de/sitecore/__/~/media/universal/logo/2015/black/00.svg" alt="EF Education First" />
-        </div>
-        <div className="applicationNameDiv1"><h3>Program Guide 2.0</h3></div>
-        {/* Navigation Bar Menu */}
-        <div className="navItemsDiv">
-            <div className="emptyDiv"></div>
-            <div className="navBarDiv">
-                <ul id="horizontal-style">
-                    <li>
-                        <Link to="/SearchResults" class="nav-link" >Search Results <span class="sr-only">(current)</span></Link>
-                    </li>
-                    <li>
-                        <Link to="/ComparePages" class="nav-link">Compare Pages</Link>
-                    </li>
-                    <li >
-                        <Link to="/AddUser" class="nav-link">Add User</Link>
-                    </li>
-                    <li>
-                        <Link to="/BulkUpload" class="nav-link">Bulk Upload</Link>
-                    </li>
-                    <li >
-                        <select class="form-control" id="exampleFormControlSelect1">
-                            <option value="select">---Choose---</option>
-                        </select>
-                    </li>
-                </ul>
-                {/* <Routes /> */}
-            </div>
-        </div>
-        {/* Display Profile Information */}
-        <div className="profileDiv">
-            <p>User Name <span className="floatLeft"></span ></p>
-        </div>
-    </div>
-    );
-  }
-}
-
-export default Header;
+        return (
+            <div className="headerDiv">
+                {/* Display Brand */}
+                <div className="brandDiv">
+                    <nav class="navbar navbar-light bg-light">
+                        <img className="efLogo" src="//www.ef.de/sitecore/__/~/media/universal/logo/2015/black/00.svg" alt="EF Education First" />
+                        <strong> Program Guide 2.0 </strong>
+                    </nav>
+                </div>
+                {/* <div className="applicationNameDiv1"><h3>Program Guide 2.0</h3></div> */}
+                {/* Navigation Bar Menu */}
+                <div className="navItemsDiv">
+                    {/* <div className="emptyDiv"></div> */}
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item active">
+                                    <Link to="/SearchResults" class="nav-link" >Search Results <span class="sr-only">(current)</span></Link>
+                                </li>
+                                <li class="nav-item">
+                                    <Link to="/ComparePages" class="nav-link">Compare Pages</Link>
+                                </li>
+                                <li class="nav-item">
+                                    <Link to="/AddUser" class="nav-link">Add User</Link>
+                                </li>
+                                <li class="nav-item">
+                                    <Link to="/BulkUpload" class="nav-link">Bulk Upload</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <select class="form-control" id="exampleFormControlSelect1">
+                                        <option value="select">---Choose---</option>
+                                    </select>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                    {/* <Routes /> */}
+                </div>
+                {/* Display Profile Information */}
+                <div className="profileDiv">
+                <nav class="navbar navbar-light bg-light">
+                karthik.subbarayappa
+                    {/* <p>User Name <span className="floatLeft"></span ></p>
+                    <img src={LogoutIcon} alt="Logo" /> */}
+                        <img src={LogoutIcon}  alt=""/>
+                    </nav>
+                </div>
+                </div>
+                );
+            }
+        }
+        
+        export default Header;
