18fccb86aeb0a75017b9792f6759ccb6f21c654b
Vivek Siddappa <vivek.siddappa@ef.com>
2018-08-09 15:03:15 +0530

    Added Images

--- ProgramGuideUI/src/Components/UserManagement/AddUserPopup.js
+++ ProgramGuideUI/src/Components/UserManagement/AddUserPopup.js
@@ -14,19 +14,20 @@ class AddUserPopup extends Component {
 
    
     render() {
+        let type = this.props.Operations;
         return (
             <div className="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true" data-backdrop="static" data-keyboard="false">
                 <div className="modal-dialog modal-lg" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
                             <br />
-                            Add User
+                            {type}
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={this.handleCloseClick}>
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div class="modal-body">
-                           
+                           {type === 'Add User'? 'Add User': type === 'Edit User' ? 'Edit User' : 'Delete User'}
                         </div>
                         <div class="modal-footer">
                             <button type="button" className="btn btn-primary">Save</button>
