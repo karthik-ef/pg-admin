cf8d634de30c0ab04df487b9e4634c9cb2401bb9
Karthik Subbarayappa <karthik.subbarayappa@ef.com>
2018-08-20 16:12:21 +0530

    Create Page Validation

--- ProgramGuideUI/src/Components/CreatePage.js
+++ ProgramGuideUI/src/Components/CreatePage.js
@@ -1,44 +1,77 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import EditContent from './SearchResult/EditContent';
+
+var PageUrl = '';
 
 class CreatePage extends Component {
 
   constructor() {
-    super();this.CreatePage = this.CreatePage.bind(this);
+    super(); this.CreatePage = this.CreatePage.bind(this);
     this.state = {
-      showModal: false
+      uniqueResult: [],
+      showModal: false,
+      showErrorMessage: false 
     }
   }
 
-  CreatePage() {
-    this.setState({ showModal: true });
+  componentDidMount(){
+    this.getUniqueContentData();
+  }
+
+  getUniqueContentData() {
+    $.ajax({
+      url: 'http://localhost:3001/getUniqueContentResults/?marketCode=es',
+      type: 'GET',
+      cache: false,
+      success: function (data) {
+        this.setState({ uniqueResult: data });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.log(err);
+      }
+    });
+
+  }
+
+  CreatePage(e) {
+    this.PageUrl = this.refs.ParentPageUrl.value;
+    if (!this.state.uniqueResult.filter(m => m.PageUrl === this.PageUrl).length > 0) {
+      this.setState({ showErrorMessage: false, showModal: true });
+    }
+    else {
+      this.setState({ showErrorMessage: true });
+    }
+    e.preventDefault();
   }
 
   dataFromEditContent = (value) => {
-    if (value){
-        this.setState({showModal: !this.state.showModal})
+    if (value) {
+      this.setState({ showModal: !this.state.showModal })
     }
-}
+  }
 
   render() {
     return (
       <div className="itemDiv">
-        {this.state.showModal ? <EditContent callbackFromEditContent={this.dataFromEditContent} /> : ''}
+        {this.state.showModal ? <EditContent PageUrl = {this.PageUrl} callbackFromEditContent={this.dataFromEditContent} /> : ''}
         <strong> Create Page URL: </strong>
         <br />
         <div class="row">
           <div class="col-sm-12">
+          <form onSubmit={this.CreatePage.bind(this)}>
             <div class="input-group input-group-sm">
-              <input type="text" class="form-control input-sm" id="search-church" ref="ParentPageUrl"
-                defaultValue='' required />
+              <input type="text" class="form-control input-sm" ref="ParentPageUrl" required />
               <span class="input-group-btn">
-                <button class="btn btn-primary btn-sm" type="submit" onClick={this.CreatePage}>Create Page</button>
+                <button class="btn btn-primary btn-sm" type="submit" >Create Page</button>
               </span>
             </div>
+            </form>
             <br />
-            <div class="alert alert-danger" role="alert">
-              Page URL - <strong>/pg/</strong> for Market <strong>ES</strong> already exisit! Please choose different URL or Market
-          </div>
+            {this.state.showErrorMessage ? <div class="alert alert-danger" role="alert">
+              Page URL - <strong>{this.PageUrl}</strong> for Market <strong>ES</strong> already exisit! Please choose different URL or Market
+          </div> : ''}
+            
           </div>
         </div>
       </div>
--- ProgramGuideUI/src/Components/SearchResult/EditContent.js
+++ ProgramGuideUI/src/Components/SearchResult/EditContent.js
@@ -96,7 +96,7 @@ class EditContent extends Component {
                     <div class="modal-dialog modal-lg" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <strong> URL: {EditPage['PageUrl']} </strong>
+                                <strong> URL: {EditPage['PageUrl'] === undefined ? this.props.PageUrl : EditPage['PageUrl']} </strong>
 
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close" onClick={this.handleCloseClick}>
                                     <span aria-hidden="true">&times;</span>
