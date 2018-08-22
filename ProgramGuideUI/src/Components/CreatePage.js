bc54d1fdbe360d8cea48447eabbb8b11c027e804
Karthik Subbarayappa <karthik.subbarayappa@ef.com>
2018-08-22 13:33:59 +0530

    CreatePage validation

--- ProgramGuideUI/src/Components/CreatePage.js
+++ ProgramGuideUI/src/Components/CreatePage.js
@@ -7,15 +7,18 @@ var PageUrl = '';
 class CreatePage extends Component {
 
   constructor() {
-    super(); this.CreatePage = this.CreatePage.bind(this);
+    super();
+    this.CreatePage = this.CreatePage.bind(this);
+    this.OnBlur = this.OnBlur.bind(this);
     this.state = {
       uniqueResult: [],
       showModal: false,
-      showErrorMessage: false 
+      showErrorMessage: false,
+      showValidationError: false
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getUniqueContentData();
   }
 
@@ -35,14 +38,15 @@ class CreatePage extends Component {
   }
 
   CreatePage(e) {
-    this.PageUrl = this.refs.ParentPageUrl.value;
-    if (!this.state.uniqueResult.filter(m => m.PageUrl === this.PageUrl).length > 0) {
-      this.setState({ showErrorMessage: false, showModal: true });
-    }
-    else {
-      this.setState({ showErrorMessage: true });
-    }
-    e.preventDefault();
+    // this.PageUrl = this.refs.ParentPageUrl.value;
+    // alert(this.PageUrl);
+    // if (!this.state.uniqueResult.filter(m => m.PageUrl === this.PageUrl).length > 0) {
+    //   this.setState({ showErrorMessage: false, showModal: true });
+    // }
+    // else {
+    //   this.setState({ showErrorMessage: true });
+    // }
+    // e.preventDefault();
   }
 
   dataFromEditContent = (value) => {
@@ -51,27 +55,46 @@ class CreatePage extends Component {
     }
   }
 
+  OnBlur() {
+    this.PageUrl = this.refs.ParentPageUrl.value;
+    var validate = !/\s/.test(this.refs.ParentPageUrl.value) && /^\//.test(this.refs.ParentPageUrl.value) && this.refs.ParentPageUrl.value.endsWith('/');
+    if (!validate) {
+      this.setState({ showValidationError: true, showErrorMessage: false, showModal: false });
+    }
+    else if(this.state.uniqueResult.filter(m => m.PageUrl === this.PageUrl).length > 0){
+      this.setState({ showValidationError: false, showErrorMessage: true, showModal: false });
+    }
+    else {
+      this.setState({ showValidationError: false, showErrorMessage: false, showModal: true });
+    }
+
+    //var t = validate.toString().matches("\\s");
+    // console.log(/\s/.test(validate));
+  }
+
   render() {
     return (
       <div className="itemDiv">
-        {this.state.showModal ? <EditContent PageUrl = {this.PageUrl} callbackFromEditContent={this.dataFromEditContent} /> : ''}
+        {this.state.showModal ? <EditContent PageUrl={this.PageUrl} callbackFromEditContent={this.dataFromEditContent} /> : ''}
         <strong> Create Page URL: </strong>
         <br />
         <div class="row">
           <div class="col-sm-12">
-          <form onSubmit={this.CreatePage.bind(this)}>
-            <div class="input-group input-group-sm">
-              <input type="text" class="form-control input-sm" ref="ParentPageUrl" required />
-              <span class="input-group-btn">
-                <button class="btn btn-primary btn-sm" type="submit" >Create Page</button>
-              </span>
-            </div>
-            </form>
+              <div class="input-group input-group-sm">
+                <input type="text" class="form-control input-sm" onBlur={this.OnBlur} ref="ParentPageUrl" required />
+                <span class="input-group-btn">
+                  <button class="btn btn-primary btn-sm" type="submit" >Create Page</button>
+                </span>
+              </div>
             <br />
             {this.state.showErrorMessage ? <div class="alert alert-danger" role="alert">
               Page URL - <strong>{this.PageUrl}</strong> for Market <strong>ES</strong> already exisit! Please choose different URL or Market
           </div> : ''}
-            
+
+            {this.state.showValidationError ? <div class="alert alert-danger" role="alert">
+              Please provide Valid Page URL
+          </div> : ''}
+
           </div>
         </div>
       </div>
