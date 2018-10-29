ae1b801157c419d58b2cbe012ff9d8702181b02e
Karthik Subbarayappa <karthik.subbarayappa@ef.com>
2018-10-29 13:06:45 +0530

    Nav bar design changes

--- ProgramGuideUI/src/Components/Dashboard/Header.js
+++ ProgramGuideUI/src/Components/Dashboard/Header.js
@@ -24,7 +24,7 @@ class Header extends Component {
     }
 
     componentDidMount() {
-        userName = JSON.parse(localStorage.getItem('Login'))['UserName']; 
+        userName = JSON.parse(localStorage.getItem('Login'))['UserName'];
         role = JSON.parse(localStorage.getItem('Login'))['Roles']['RoleName'];
         this.getAvailableMarket();
     }
@@ -46,12 +46,12 @@ class Header extends Component {
 
     getUserMarkets() {
         $.ajax({
-            url: 'http://ctdev.ef.com:3000/userMarkets/?userName=' + userName ,
+            url: 'http://ctdev.ef.com:3000/userMarkets/?userName=' + userName,
             type: 'GET',
             cache: false,
             success: function (data) {
                 userMarkets = data;
-                this.setState({availableMarkets: userMarkets.filter(m => uniqueContentMarkets.map(m => {return m.MarketCode}).includes(m.MarketCode))});
+                this.setState({ availableMarkets: userMarkets.filter(m => uniqueContentMarkets.map(m => { return m.MarketCode }).includes(m.MarketCode)) });
             }.bind(this),
             error: function (xhr, status, err) {
                 console.log(err);
@@ -62,21 +62,21 @@ class Header extends Component {
     bindUserMarkets() {
         if (this.state.availableMarkets) {
             return this.state.availableMarkets.sort((a, b) => a.Name.localeCompare(b.Name))
-            .map(m => { return <option key={m.MarketCode} value={m.MarketCode} selected = {localStorage.getItem('Market') === m.MarketCode ? true : false}>{m.Name} </option> });
+                .map(m => { return <option key={m.MarketCode} value={m.MarketCode} selected={localStorage.getItem('Market') === m.MarketCode ? true : false}>{m.Name} </option> });
         }
     }
 
-    onChange(){
-        this.setState({ loading: true },function(){
+    onChange() {
+        this.setState({ loading: true }, function () {
             this.props.opac(this.state.loading);
         });
         localStorage.setItem('Market', this.refs.SelectedMarket.value);
-        if (window.location.pathname === '/SearchResults' || window.location.pathname === '/CreatePage' ){
+        if (window.location.pathname === '/SearchResults' || window.location.pathname === '/CreatePage') {
             window.location.reload();
         }
     }
 
-    Logout(){
+    Logout() {
         window.location.pathname = '/';
         localStorage.clear();
     }
@@ -87,62 +87,98 @@ class Header extends Component {
         return (
             <div className="headerDiv">
 
-                {/* Display Brand */}
-                <div className="brandDiv">
-                    <nav className="navbar navbar-light bg-light">
-                        <img className="efLogo" src="//www.ef.de/sitecore/__/~/media/universal/logo/2015/black/00.svg" alt="EF Education First" />
-                        <strong> Program Guide 2.0 </strong>
-                    </nav>
-                </div>
+                <div className="test1">
+                    {/* Display Brand */}
+                    <div className="brandDiv">
+                        <nav className="navbar navbar-light bg-light">
+                            <img className="efLogo" src="//www.ef.de/sitecore/__/~/media/universal/logo/2015/black/00.svg" alt="EF Education First" />
+                        </nav>
+                    </div>
 
-                {/* Navigation Bar Menu */}
-                <div className="navItemsDiv">
-                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav mr-auto">
-                                <li className="nav-item active">
-                                    <Link to="/SearchResults" className="nav-link" >Search Results <span className="sr-only">(current)</span></Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to="/CreatePage" className="nav-link">Create Page</Link>
-                                </li>
-                                {role !== 'Admin' ? '' : <li className="nav-item">
-                                    <Link to="/AddUser" className="nav-link">Add User</Link>
-                                </li>}
-                                <li className="nav-item">
-                                    <Link to="/BulkUpload" className="nav-link">Bulk Upload</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <select className="form-control" id="exampleFormControlSelect1" ref="SelectedMarket" onChange= {this.onChange.bind(this)}>
-                                        {this.state.availableMarkets.length !== 1 ? <option value="select">---Choose Market---</option> : ''}
-                                        {this.state.availableMarkets.length === 1 ? localStorage.setItem('Market', this.state.availableMarkets.map(m => {return m.MarketCode})) : ''}
-                                        {this.bindUserMarkets()}
-                                    </select>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
+                    {/* Navigation Bar Menu */}
+                    <div className="navItemsDiv">
+                        <nav className="navbar navbar-light bg-light">
+                            <select className="form-control" id="exampleFormControlSelect1" ref="SelectedMarket" onChange={this.onChange.bind(this)}>
+                                {this.state.availableMarkets.length !== 1 ? <option value="select">---Choose Market---</option> : ''}
+                                {this.state.availableMarkets.length === 1 ? localStorage.setItem('Market', this.state.availableMarkets.map(m => { return m.MarketCode })) : ''}
+                                {this.bindUserMarkets()}
+                            </select>
+                        </nav>
 
-                {/* Display Profile Information */}
-                <div className="profileDiv">
-                    <nav className="navbar navbar-light bg-light">
-                        {userName}
-                        <button type="button" class="btn btn-primary" onClick = {this.Logout.bind(this)}>Logout</button>
-                        {/* karthik.subbarayappa
+                    </div>
+                    {/* <div className="navItemsDiv">
+                        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul className="navbar-nav mr-auto">
+                                    <li className="nav-item active">
+                                        <Link to="/SearchResults" className="nav-link" >Search Results <span className="sr-only">(current)</span></Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/CreatePage" className="nav-link">Create Page</Link>
+                                    </li>
+                                    {role !== 'Admin' ? '' : <li className="nav-item">
+                                        <Link to="/AddUser" className="nav-link">Add User</Link>
+                                    </li>}
+                                    <li className="nav-item">
+                                        <Link to="/BulkUpload" className="nav-link">Bulk Upload</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <select className="form-control" id="exampleFormControlSelect1" ref="SelectedMarket" onChange={this.onChange.bind(this)}>
+                                            {this.state.availableMarkets.length !== 1 ? <option value="select">---Choose Market---</option> : ''}
+                                            {this.state.availableMarkets.length === 1 ? localStorage.setItem('Market', this.state.availableMarkets.map(m => { return m.MarketCode })) : ''}
+                                            {this.bindUserMarkets()}
+                                        </select>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div> */}
+
+                    {/* Display Profile Information */}
+                    <div className="profileDiv">
+                        <nav className="navbar navbar-light bg-light">
+                            {userName}
+                            <button type="button" class="btn btn-primary" onClick={this.Logout.bind(this)}>Logout</button>
+                            {/* karthik.subbarayappa
                         <img src={LogoutIcon} alt="" /> */}
-                    </nav>
+                        </nav>
+                    </div>
+                    <div>
+                        <ClipLoader
+                            className={override}
+                            sizeUnit={"px"}
+                            size={50}
+                            color={'#123abc'}
+                            loading={this.state.loading}
+                        />
+                    </div>
                 </div>
-                <div>
-                    <ClipLoader
-                    className={override}
-                    sizeUnit={"px"}
-                    size={50}
-                    color={'#123abc'}
-                    loading={this.state.loading}
-                    />
+                <div className="test2">
+                    <strong> Program Guide 2.0 </strong>
+                    <div className="navItemsDiv1">
+                        <nav className="navbar navbar-expand-lg navbar-light">
+                            <br />
+                            <div className="collapse navbar-collapse" id="navbarSupportedContent1">
+                                <ul className="navbar-nav mr-auto">
+                                    <li className="nav-item active">
+                                        <Link to="/SearchResults" className="nav-link" >Search Results <span className="sr-only">(current)</span></Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/CreatePage" className="nav-link">Create Page</Link>
+                                    </li>
+                                    {role !== 'Admin' ? '' : <li className="nav-item">
+                                        <Link to="/AddUser" className="nav-link">Add User</Link>
+                                    </li>}
+                                    <li className="nav-item">
+                                        <Link to="/BulkUpload" className="nav-link">Bulk Upload</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
                 </div>
-            </div>
+
+            </div >
         );
     }
 }
