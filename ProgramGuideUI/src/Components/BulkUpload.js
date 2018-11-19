16e18e783a7577740f29c622d2a7f11ccfb2440f
Karthik Subbarayappa <karthik.subbarayappa@ef.com>
2018-11-19 14:40:27 +0530

    fetchBulkUploadDetails and display

--- ProgramGuideUI/src/Components/BulkUpload.js
+++ ProgramGuideUI/src/Components/BulkUpload.js
@@ -3,296 +3,298 @@ import readXlsxFile from 'read-excel-file';
 import jsonxml from 'jsontoxml';
 import $ from 'jquery';
 
-let h ;
-var UpdatedUniqueContentId = []
-
-
 class BulkUpload extends Component {
 
-  constructor(){
+  constructor() {
     super();
+    this.state = {
+      bulkUploadDetailsData: []
+    }
     this.h = {}
-    this.handleUploadFile=this.handleUploadFile.bind(this);
-    
+    this.handleUploadFile = this.handleUploadFile.bind(this);
+
   }
 
- 
-  
-  handleUploadFile(){
-   function escapeXml(unsafe) {
-    
+
+  handleUploadFile() {
+    function escapeXml(unsafe) {
+
       return unsafe.replace(/[<>&'"]/g, function (c) {
-          switch (c) {
-              case '<': return '&lt;';
-              case '>': return '&gt;';
-              case '&': return '&amp;';
-              case '\'': return '&apos;';
-              case '"': return '&quot;';
-          }
+        switch (c) {
+          case '<': return '&lt;';
+          case '>': return '&gt;';
+          case '&': return '&amp;';
+          case '\'': return '&apos;';
+          case '"': return '&quot;';
+        }
       });
     }
     const schema = {
-      'Uniquecontent':{
-      prop: 'Uniquecontent',
-          type: {
-      
-      'UniqueContent_ID': {
-        prop: 'UniqueContent_ID',
-        type: Number,
-        required: true
-      },
-      'MarketCode': {
-        prop: 'MarketCode',
-        type: String,
-        required: true
-      },
+      'Uniquecontent': {
+        prop: 'Uniquecontent',
+        type: {
 
-	    'PageUrl': {
-        prop: 'PageUrl',
-        type: String,
-        required: true
-      },
+          'UniqueContent_ID': {
+            prop: 'UniqueContent_ID',
+            type: Number,
+            required: true
+          },
+          'MarketCode': {
+            prop: 'MarketCode',
+            type: String,
+            required: true
+          },
 
-      'Tag_Topic': {
-        prop: 'Tag_Topic',
-        type: String,
-        required: true,
-      },
-	    'Tag_When': {
-        prop: 'Tag_When',
-        type: String,
-        required: false
-      },
-      'Tag_CourseType': {
-        prop: 'Tag_CourseType',
-        type: String,
-        required: true
-      },
-	    'Tag_AgeRange': {
-        prop: 'Tag_AgeRange',
-        type: String,
-        required: false
-      },
-	  	  'Tag_Duration': {
-        prop: 'Tag_Duration',
-        type: String,
-        required: true
-      },
-      'AdditionalDurationDetails': {
-        prop: 'AdditionalDurationDetails',
-        type: String,
-        required: true
-      },
-	    'Tag_LanguageOfInstruction': {
-        prop: 'Tag_LanguageOfInstruction',
-        type: String,
-        required: false
-      },
-      'Tag_LanguageLearned': {
-        prop: 'Tag_LanguageLearned',
-        type: String,
-        required: true
-      },
-	    'Tag_Platform': {
-        prop: 'Tag_Platform',
-        type: String,
-        required: false
-      },
-	  	    'Tag_Continent': {
-        prop: 'Tag_Continent',
-        type: String,
-        required: true
-      },
-	    'Tag_Country': {
-        prop: 'Tag_Country',
-        type: String,
-        required: false
-      },
-      'Tag_State': {
-        prop: 'Tag_State',
-        type: String,
-        required: true
-      },
-	    'Tag_City': {
-        prop: 'Tag_City',
-        type: String,
-        required: false
-      },
-	  	    'Tag_Feature': {
-        prop: 'Tag_Feature',
-        type: String,
-        required: true
-      },
-	    'BannerImage': {
-        prop: 'BannerImage',
-        type: String,
-        required: false
-      },
-	    'MetaTitle': {
-        prop: 'MetaTitle',
-        type: String,
-        required: true,
-        parse(value) {
-          return escapeXml(value);
-            
-          }
-      },
-	    'MetaDescription': {
-        prop: 'MetaDescription',
-        type: String,
-        required: false,
-        parse(value) {
-          return escapeXml(value);
-            
-          }
-      },
-	  	    'MetaRobot': {
-        prop: 'MetaRobot',
-        type: String,
-        required: true,
-        parse(value) {
-          return escapeXml(value);
-            
-          }
-      },
-	    'PageTitle': {
-        prop: 'PageTitle',
-        type: String,
-        required: false,
-        parse(value) {
-          return escapeXml(value);
-            
-          }
-      },
-      'VisibleIntroText': {
-        prop: 'VisibleIntroText',
-        type: String,
-        required: true,
-       
-        parse(value) {
-          return escapeXml(value);
-            
-          }
-          
-      },
-	    'HiddenIntroText': {
-        prop: 'HiddenIntroText',
-        type: String,
-        required: false,
-        parse(value) {
-          return escapeXml(value);
-            
-          }
-      },
-	  	    'SubHeader1': {
-        prop: 'SubHeader1',
-        type: String,
-        required: true,
-        parse(value) {
-          return escapeXml(value);
-            
-          }
-        
-      },
-	    'SubHeader2': {
-        prop: 'SubHeader2',
-        type: String,
-        required: false,
-        parse(value) {
-          return escapeXml(value);
-            
-          }
-      },
-      'ContentText1': {
-        prop: 'ContentText1',
-        type: String,
-        required: true,
-        parse(value) {
-          return escapeXml(value);
-            
-          }
-      },
-	    'ContentText2': {
-        prop: 'ContentText2',
-        type: String,
-        required: false,
-        parse(value) {
-          return escapeXml(value);
-            
-          }
-      },
-	  	    'BreadcrumbText': {
-        prop: 'BreadcrumbText',
-        type: String,
-        required: true,
-        parse(value) {
-          return escapeXml(value);
-            
-          }
-        },
+          'PageUrl': {
+            prop: 'PageUrl',
+            type: String,
+            required: true
+          },
+
+          'Tag_Topic': {
+            prop: 'Tag_Topic',
+            type: String,
+            required: true,
+          },
+          'Tag_When': {
+            prop: 'Tag_When',
+            type: String,
+            required: false
+          },
+          'Tag_CourseType': {
+            prop: 'Tag_CourseType',
+            type: String,
+            required: true
+          },
+          'Tag_AgeRange': {
+            prop: 'Tag_AgeRange',
+            type: String,
+            required: false
+          },
+          'Tag_Duration': {
+            prop: 'Tag_Duration',
+            type: String,
+            required: true
+          },
+          'AdditionalDurationDetails': {
+            prop: 'AdditionalDurationDetails',
+            type: String,
+            required: true
+          },
+          'Tag_LanguageOfInstruction': {
+            prop: 'Tag_LanguageOfInstruction',
+            type: String,
+            required: false
+          },
+          'Tag_LanguageLearned': {
+            prop: 'Tag_LanguageLearned',
+            type: String,
+            required: true
+          },
+          'Tag_Platform': {
+            prop: 'Tag_Platform',
+            type: String,
+            required: false
+          },
+          'Tag_Continent': {
+            prop: 'Tag_Continent',
+            type: String,
+            required: true
+          },
+          'Tag_Country': {
+            prop: 'Tag_Country',
+            type: String,
+            required: false
+          },
+          'Tag_State': {
+            prop: 'Tag_State',
+            type: String,
+            required: true
+          },
+          'Tag_City': {
+            prop: 'Tag_City',
+            type: String,
+            required: false
+          },
+          'Tag_Feature': {
+            prop: 'Tag_Feature',
+            type: String,
+            required: true
+          },
+          'BannerImage': {
+            prop: 'BannerImage',
+            type: String,
+            required: false
+          },
+          'MetaTitle': {
+            prop: 'MetaTitle',
+            type: String,
+            required: true,
+            parse(value) {
+              return escapeXml(value);
+
+            }
+          },
+          'MetaDescription': {
+            prop: 'MetaDescription',
+            type: String,
+            required: false,
+            parse(value) {
+              return escapeXml(value);
+
+            }
+          },
+          'MetaRobot': {
+            prop: 'MetaRobot',
+            type: String,
+            required: true,
+            parse(value) {
+              return escapeXml(value);
+
+            }
+          },
+          'PageTitle': {
+            prop: 'PageTitle',
+            type: String,
+            required: false,
+            parse(value) {
+              return escapeXml(value);
+
+            }
+          },
+          'VisibleIntroText': {
+            prop: 'VisibleIntroText',
+            type: String,
+            required: true,
+
+            parse(value) {
+              return escapeXml(value);
+
+            }
+
+          },
+          'HiddenIntroText': {
+            prop: 'HiddenIntroText',
+            type: String,
+            required: false,
+            parse(value) {
+              return escapeXml(value);
+
+            }
+          },
+          'SubHeader1': {
+            prop: 'SubHeader1',
+            type: String,
+            required: true,
+            parse(value) {
+              return escapeXml(value);
+
+            }
+
+          },
+          'SubHeader2': {
+            prop: 'SubHeader2',
+            type: String,
+            required: false,
+            parse(value) {
+              return escapeXml(value);
+
+            }
+          },
+          'ContentText1': {
+            prop: 'ContentText1',
+            type: String,
+            required: true,
+            parse(value) {
+              return escapeXml(value);
+
+            }
+          },
+          'ContentText2': {
+            prop: 'ContentText2',
+            type: String,
+            required: false,
+            parse(value) {
+              return escapeXml(value);
+
+            }
+          },
+          'BreadcrumbText': {
+            prop: 'BreadcrumbText',
+            type: String,
+            required: true,
+            parse(value) {
+              return escapeXml(value);
+
+            }
+          },
           'DrillDownAlias': {
             prop: 'DrillDownAlias',
             type: String,
-            required: true,    
-  },
-      
-	    'FeaturePageTag1': {
-        prop: 'FeaturePageTag1',
-        type: String,
-        required: false,
-        
-      },
-      'FeaturePageTag2': {
-        prop: 'FeaturePageTag2',
-        type: String,
-        required: false
-      },
-		'ParentPageID': {
-        prop: 'ParentPageID',
-        type: Number,
-        required: false
-      },
-	    
+            required: true,
+          },
+
+          'FeaturePageTag1': {
+            prop: 'FeaturePageTag1',
+            type: String,
+            required: false,
+
+          },
+          'FeaturePageTag2': {
+            prop: 'FeaturePageTag2',
+            type: String,
+            required: false
+          },
+          'ParentPageID': {
+            prop: 'ParentPageID',
+            type: Number,
+            required: false
+          },
+
+        }
       }
-    }
 
     }
-    var file = document.getElementById("input").files[0];
-
+    var file = document.getElementById("uploadBatchFile").files[0];
 
     readXlsxFile(file, { schema }).then(({ rows, errors }) => {
 
       var dataRow = [];
-       //`errors` have shape `{ row, column, error, value }`.
-       //console.log(rows[0]);
-       console.log(rows);
+      //`errors` have shape `{ row, column, error, value }`.
+      //console.log(rows[0]);
 
-       rows.forEach(element => {
+      rows.forEach(element => {
         dataRow.push(element['Uniquecontent']);
-       });
-     this.UpdatedUniqueContentId = '<BatchDetails xmlns="">' + dataRow.map(m => { return '<BatchRow UniqueContent_ID="' + m.UniqueContent_ID + '"/>' }).toString().replace(/,/g, ' ') + '</BatchDetails>'
+      });
+      this.UpdatedUniqueContentId = '<BatchDetails xmlns="">' + dataRow.map(m => { return '<BatchRow UniqueContent_ID="' + m.UniqueContent_ID + '"/>' }).toString().replace(/,/g, ' ') + '</BatchDetails>'
 
-     console.log(this.UpdatedUniqueContentId);
-     this.BulkUploadDetails = {};
+      this.BulkUploadDetails = {};
 
-     this.BulkUploadDetails.xmlData = this.UpdatedUniqueContentId;
-     this.BulkUploadDetails.userName = JSON.parse(localStorage.getItem('Login'))['UserName'];
-     
-     var xml = jsonxml({
-     
-      XmlDocument:rows
-  })
-  console.log(xml);
-  this.h.userName=JSON.parse(localStorage.getItem('Login'))['UserName'];
-  this.h.xmlData=xml.toString();
-  this.BulkUpload();
- 
+      this.BulkUploadDetails.xmlData = this.UpdatedUniqueContentId;
+      this.BulkUploadDetails.userName = JSON.parse(localStorage.getItem('Login'))['UserName'];
+
+      var xml = jsonxml({
+
+        XmlDocument: rows
+      })
+      this.h.userName = JSON.parse(localStorage.getItem('Login'))['UserName'];
+      this.h.xmlData = xml.toString();
+      this.BulkUpload();
+
     })
-    
-
-    
   }
-BulkUpload() {
+
+  componentDidMount() {
+    this.getBulkUploadDetails();
+    $('#uploadBatchFile').on('change', function () {
+      //get the file name
+      var fileName = document.getElementById('uploadBatchFile').files[0].name;
+      //replace the "Choose a file" label
+      $(this).next('.custom-file-label').html(fileName);
+    })
+  }
+
+  BulkUpload() {
     $.ajax({
       url: 'http://ctdev.ef.com:3000/BulkUpload',
       type: 'POST',
@@ -300,7 +302,7 @@ BulkUpload() {
       data: this.h,
       cache: false,
       success: function (data, status, err) {
-      this.SaveBulkUploadDetails();
+        this.SaveBulkUploadDetails();
       }.bind(this),
       error: function (xhr, status, err) {
         console.log(err);
@@ -309,7 +311,7 @@ BulkUpload() {
 
   }
 
-  SaveBulkUploadDetails(){
+  SaveBulkUploadDetails() {
     console.log(this.BulkUploadDetails);
     $.ajax({
       url: 'http://ctdev.ef.com:3000/CreateBatchDetails',
@@ -318,7 +320,9 @@ BulkUpload() {
       data: this.BulkUploadDetails,
       cache: false,
       success: function (data, status, err) {
-        console.log(data);
+        this.getBulkUploadDetails();
+        document.getElementById("uploadBatchFile").value = '';
+        document.getElementById("uploadBatchFileLabel").innerHTML = 'Choose file';
         alert('Excel uploaded sucessfully');
       }.bind(this),
       error: function (xhr, status, err) {
@@ -327,16 +331,48 @@ BulkUpload() {
     });
   }
 
-  render() {
+  getBulkUploadDetails() {
+    $.ajax({
+      url: 'http://ctdev.ef.com:3000/getBatchDetails',
+      type: 'GET',
+      cache: false,
+      success: function (data, status, err) {
+        if (data.length > 0) {
+          this.setState({
+            bulkUploadDetailsData: data.map(m => {
+              return <tr><td className="tdBulkUpload"><p className="bulkUploadContent">Batch Id# {m.BATCH_ID}, upload date {m.UploadDate}, by {m.UpdatedBy} </p></td>
+                <td className="tdBulkUpload"><button type="button" class="btn btn-primary btn-sm">Publish to LIVE</button></td>
+              </tr>
+            })
+          });
+        }
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.log(err);
+      }
+    });
+  }
 
+  render() {
     return (
-      <div className="container">
-      bulk upload
-       <div>
-      
-        <input type="file" id="input"  />
-      </div>
-      <input type="submit" value="Upload" id="tnSubmit" onClick={this.handleUploadFile}/>
+      <div className="itemDiv">
+        <div class="input-group">
+          <div class="custom-file">
+            <input type="file" class="custom-file-input" id="uploadBatchFile"/>
+            <label class="custom-file-label" id="uploadBatchFileLabel" for="inputGroupFile04">Choose file</label>
+          </div>
+          <div class="input-group-append">
+            <button class="btn btn-primary" type="button" onClick={this.handleUploadFile}>Upload</button>
+          </div>
+        </div>
+        <br />
+        {this.state.bulkUploadDetailsData.length > 0 ?
+          <table className="table table-borderless" id="bulkUploadBatchDetails">
+            <p><b>Batches not published to LIVE</b></p>
+            {this.state.bulkUploadDetailsData}
+          </table>
+          : ''
+        }
       </div>
     );
   }
