7d52c89cb9dc58bf833cb6b2ef83c7880d080eb5
Karthik Subbarayappa <karthik.subbarayappa@ef.com>
2019-02-11 16:51:37 +0530

    alert message once the file is uploaded

--- ProgramGuideUI/src/api/BulkUpload.js
+++ ProgramGuideUI/src/api/BulkUpload.js
@@ -4,6 +4,7 @@ import * as API from '../utils/endpoints';
 export function BulkUpload() {
     axios.post(API.bulkUpload, this.uploadExcelDetails)
     .then(result => {
+        alert('File is uploaded sucessfully')
         console.log(result);
        // saveBatchDetails.call(this);
     }).catch(err => { console.log(err) });
