02e95b20fe20f3832f92dc2d51ace4d195d5a10c
Karthik Subbarayappa <karthik.subbarayappa@ef.com>
2019-03-20 18:14:45 +0530

    Display publish to live pages based on user role

--- ProgramGuideUI/src/client/CustomControls/Grid.js
+++ ProgramGuideUI/src/client/CustomControls/Grid.js
@@ -9,35 +9,50 @@ class Grid extends Component {
             <Table
                 data={this.props.setData}
                 minRows={0}
-                columns={[
-                    {
-                        columns: [
-                            {
-                                Header: <strong>Page URL</strong>,
-                                id: "PageUrl",
-                                accessor: d => d.PageUrl,
-                                sortable: false
-                            },
-                            {
-                                Header: <strong>Page Title</strong>,
-                                id: "PageTitle",
-                                accessor: d => d.PageTitle,
-                                sortable: false
-                            },
-                            {
-                                Header: '',
-                                sortable: false,
-                                width: 60,
-                                Cell: row => (
-                                    <div>
-                                        <span className="floatLeft"> <img src={DeleteIcon} alt="Logo" /></span>
-                                    </div>
-                                )
+                columns={[{
+                    Header: <div>
+                        <button className="btn btn-primary" type="button" >Publish to LIVE</button>
+                    </div>,
+                    columns: [
+                        {
+                            Header: <strong>Page URL</strong>,
+                            id: "PageUrl",
+                            accessor: d => d.PageUrl,
+                            sortable: false
+                        },
+                        {
+                            Header: <strong>Page Title</strong>,
+                            id: "PageTitle",
+                            accessor: d => d.PageTitle,
+                            sortable: false
+                        },
+                        {
+                            Header: '',
+                            sortable: false,
+                            width: 60,
+                            Cell: row => (
+                                <div>
+                                    <span className="floatLeft"> <img src={DeleteIcon} alt="Logo" /></span>
+                                </div>
+                            )
 
-                            }
-                        ]
-                    }
+                        }
+                    ]
+                }
                 ]}
+                getTdProps={(state, rowInfo, column, instance) => {
+                    return {
+                        onClick: (e, handleOriginal) => {
+
+                            console.log(rowInfo['original']);
+                            if (column['Header']['props'] === undefined) {
+                                //alert('delete');
+                                this.props.DeletedRow(rowInfo['original']);
+                                //this.props.DeleteRow(rowInfo['original']);
+                            }
+                        }
+                    };
+                }}
 
                 defaultPageSize={10}
                 showPageSizeOptions={false}
--- ProgramGuideUI/src/client/Publish/PublishToLive.js
+++ ProgramGuideUI/src/client/Publish/PublishToLive.js
@@ -4,8 +4,15 @@ import * as Path from '../../utils/routepath';
 import * as API from '../../api/Publish';
 import { connect } from 'react-redux';
 import Grid from '../CustomControls/Grid';
+import * as Constant from '../../utils/constant';
 
 class Content extends React.Component {
+
+    constructor() {
+        super();
+        this.gridData = [];
+        this.currentMarket = '';
+    }
 
     componentDidMount() {
         this.sucessMessage = false;
@@ -14,7 +21,7 @@ class Content extends React.Component {
 
     publishToLive() {
 
-        API.publishToLive.call(this);
+        // API.publishToLive.call(this);
         //Check if any news pages are created after the last publish
         if (this.newlyCreatedPages.length > 0) {
             this.newlyCreatedPageDetails = {};
@@ -22,34 +29,68 @@ class Content extends React.Component {
             this.newlyCreatedPageDetails.pageDetails = this.newlyCreatedPages;
 
             //Send email notification to the users
-            API.SendNotification.call(this);
+            // API.SendNotification.call(this);
         }
     }
 
-    render() {
+    DeletedPages(page) {
+        this.gridData = this.gridData.filter(m => m.UniqueContent_ID !== page.UniqueContent_ID);
+        this.setState({});
+    }
 
-        // console.log(this.props.storeData._userDashboardData
-        //     .filter(m => ['VE'].includes(m.MarketCode))
-        //     .map(m => {return m.UserName + '@ef.com'}).toString());
+    render() {
+        console.log(this.props.storeData._loginDetails.roleName);
+        console.log(this.gridData = this.props.storeData._uniqueContentData.filter(m => m.UpdateBy === this.props.storeData._loginDetails.userName && m.MarketCode === this.props.storeData._selectedMarket));
+
+        this.gridData.length === 0 || this.currentMarket !== this.props.storeData._selectedMarket
+            ? this.props.storeData._loginDetails.roleName === 'Admin' // Publish Data for Admin Users
+                ? this.gridData = this.props.storeData._uniqueContentData.filter(m => m.MarketCode === this.props.storeData._selectedMarket)
+                : this.props.storeData._loginDetails.roleName === 'General' // Publish Data for General Users
+                    ? this.gridData = this.props.storeData._uniqueContentData.filter(m => m.UpdateBy === this.props.storeData._loginDetails.userName && m.MarketCode === this.props.storeData._selectedMarket)
+                    : this.props.storeData._loginDetails.roleName === 'Power' // Publish Data for Power Users
+                        ? this.gridData = this.props.storeData._uniqueContentData.filter(m => m.MarketCode === this.props.storeData._selectedMarket)
+                        : ''
+            : ''
+
+        this.currentMarket = this.props.storeData._selectedMarket;
+
         return (
             <div className="itemDiv add-users__wrapper">
                 <div className="container">
-                    {this.sucessMessage
+                    {this.sucessMessage //Display success message after publishing the data
                         ? <div className="alert alert-success" role="alert">
                             <p>Changes are successfully publish to live. It may take few minutes to refresh the database.</p>
                         </div>
-                        :
-                        <div className="container">
-                            {this.props.storeData._loginDetails.userName.toString().toLowerCase() === 'hao.peng' ||
-                                this.props.storeData._loginDetails.userName.toString().toLowerCase() === 'ruobing.ai'
-                                ? <button className="btn btn-primary" type="button" onClick={this.publishToLive.bind(this)}>Publish All</button>
-                                : ''}
-                            <Grid setData = {this.props.storeData._uniqueContentData.filter(m => m.UpdateBy === this.props.storeData._loginDetails.userName)}/>
-
-                        </div>
+                        : !this.props.storeData._selectedMarket //Check if market is selected
+                            ? <div className="itemDiv search__results--wrapper">
+                                <div className="container">
+                                    <div className="alert alert-danger" role="alert">
+                                        <p>{Constant.ERROR_SELECT_MARKET}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            : <div className="container">
+                                {this.gridData.length === 0
+                                    ? <div className="alert alert-danger" role="alert">
+                                        <p>No Data available to publish.</p>
+                                    </div>
+                                    : <div>
+                                        <div className="alert alert-info" role="alert">
+                                            <p className="alert-text__info strong__text"> {this.gridData.length} pages will be published to LIVE. </p>
+                                        </div>
+                                        <br />
+                                        {this.props.storeData._loginDetails.userName.toString().toLowerCase() === 'hao.peng' ||
+                                            this.props.storeData._loginDetails.userName.toString().toLowerCase() === 'ruobing.ai'
+                                            ? <button className="btn btn-primary" type="button" onClick={this.publishToLive.bind(this)}>Publish All</button>
+                                            : ''}
+                                        <Grid DeletedRow={this.DeletedPages.bind(this)} setData={this.gridData} />
+                                        {/* <button className="btn btn-primary" type="button" onClick={this.publishToLive.bind(this)}>Publish to LIVE</button> */}
+                                    </div>
+                                }
+                            </div>
                     }
                 </div>
-            </div>
+            </div >
         );
     }
 }
