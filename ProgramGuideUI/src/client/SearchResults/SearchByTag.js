1e401dbf65ef2063f79a81a924fe1fa7ff718a8a
Karthik Subbarayappa <karthik.subbarayappa@ef.com>
2019-03-29 12:48:25 +0530

    CONTENT-14780: Create page tag value changes

--- ProgramGuideUI/src/client/SearchResults/SearchByTag.js
+++ ProgramGuideUI/src/client/SearchResults/SearchByTag.js
@@ -24,20 +24,47 @@ class SearchByTag extends Component {
     componentDidMount() {
         $('#exampleModalLong').modal('show');
 
-        Constant.Tag_Collection.forEach(element => {
-            element === Constant.Tag_Duration // Drop down data for duration tag
-                ? this[element] = dataForDropDown(this.props.storeData._durationTags.map(m => { return { Value: m } }))
-                : element === Constant.Tag_Platform // Drop down data for platform tag
-                    ? this[element] = dataForDropDown(this.props.storeData._platformTags.map(m => { return { Value: m } }))
-                    //Else drop down data for rest of the tags
-                    : window.location.pathname === Path.ExportPgData //drop down data for other tags for user markets
-                        ? this[element] = dataForDropDown(this.props.storeData._uniqueContentTags.filter(m => m.TagName === element)
-                            .map(m => { return m.Value })
-                            .filter((x, i, a) => { return a.indexOf(x) === i }).map(m => { return { Value: m } }))
-                        //Else drop down data for other tags only for selected market
-                        : this[element] = dataForDropDown(this.props.storeData._uniqueContentTags.filter(m => m.TagName === element && m.MarketCode === this.props.storeData._selectedMarket)
+        var ageRangeData = []
+        //Generate tag value for AgeRange
+        for (var x = 0; x < Constant.Age_Range.length; x++) {
+            for (var y = 1; y < Constant.Age_Range.length; y++) {
+                if (Constant.Age_Range[x] < Constant.Age_Range[y]) {
+                    var startAgeRange = Constant.Age_Range[x] !== 0 && Constant.Age_Range[x] < 10 ? '0' + Constant.Age_Range[x] : Constant.Age_Range[x];
+                    var endAgeRange = Constant.Age_Range[y] < 10 ? '0' + Constant.Age_Range[y] : Constant.Age_Range[y];
+                    ageRangeData.push({
+                        TagName: Constant.Tag_AgeRange,
+                        Value: startAgeRange + '-' + endAgeRange
+                    })
+                }
+            }
+        }
+
+        this.props.ValueFromDb && this.props.ValueFromDb.length === 0
+            //Tag values for create page
+            ? Constant.Tag_Collection.forEach(element => {
+                element === Constant.Tag_Topic // Tag_Topic value is market specific
+                    ? this[element] = dataForDropDown(this.props.storeData._uniqueContentTags.filter(m => m.TagName === element && m.MarketCode === this.props.storeData._selectedMarket)
+                        .map(m => { return { Value: m.Value } }))
+                    : element === Constant.Tag_AgeRange //Tag_AgeRange is combination of value from 0-99
+                        ? this[element] = dataForDropDown(ageRangeData.map(m => { return { Value: m.Value } }))
+                        : this[element] = dataForDropDown(this.props.storeData._createPageTags.filter(m => m.TagName === element)
                             .map(m => { return { Value: m.Value } }))
-        });
+            })
+            //Tag values for filter and content editor pages
+            : Constant.Tag_Collection.forEach(element => {
+                element === Constant.Tag_Duration // Drop down data for duration tag
+                    ? this[element] = dataForDropDown(this.props.storeData._durationTags.map(m => { return { Value: m } }))
+                    : element === Constant.Tag_Platform // Drop down data for platform tag
+                        ? this[element] = dataForDropDown(this.props.storeData._platformTags.map(m => { return { Value: m } }))
+                        //Else drop down data for rest of the tags
+                        : window.location.pathname === Path.ExportPgData //drop down data for other tags for user markets
+                            ? this[element] = dataForDropDown(this.props.storeData._uniqueContentTags.filter(m => m.TagName === element)
+                                .map(m => { return m.Value })
+                                .filter((x, i, a) => { return a.indexOf(x) === i }).map(m => { return { Value: m } }))
+                            //Else drop down data for other tags only for selected market
+                            : this[element] = dataForDropDown(this.props.storeData._uniqueContentTags.filter(m => m.TagName === element && m.MarketCode === this.props.storeData._selectedMarket)
+                                .map(m => { return { Value: m.Value } }))
+            });
 
         this.buildDropDownComponent();
     }
@@ -99,21 +126,21 @@ class SearchByTag extends Component {
     }
 
     render() {
-            return (
-                <div >
-                    <br />
-                    <p>{Constant.SEARCH_TAG_HEADER}</p>
-                    <div className="drop__selectors">
-                        {this.tagComponent}
-                    </div>
-                    <br />
-                    <strong>{Constant.SELECTED_TAG_HEADER}</strong>
-                    <br />
-                    <label id="selectedTags">
-                        {`${this.Tag_Topic_Value}_${this.Tag_When_Value}_${this.Tag_CourseType_Value}_${this.Tag_AgeRange_Value}_${this.Tag_Duration_Value}_${this.Tag_LanguageOfInstruction_Value}_${this.Tag_LanguageLearned_Value}_${this.Tag_Platform_Value}_${this.Tag_Continent_Value}_${this.Tag_Country_Value}_${this.Tag_State_Value}_${this.Tag_City_Value}_${this.Tag_Feature_Value}`}
-                    </label>
+        return (
+            <div >
+                <br />
+                <p>{Constant.SEARCH_TAG_HEADER}</p>
+                <div className="drop__selectors">
+                    {this.tagComponent}
                 </div>
-            );
+                <br />
+                <strong>{Constant.SELECTED_TAG_HEADER}</strong>
+                <br />
+                <label id="selectedTags">
+                    {`${this.Tag_Topic_Value}_${this.Tag_When_Value}_${this.Tag_CourseType_Value}_${this.Tag_AgeRange_Value}_${this.Tag_Duration_Value}_${this.Tag_LanguageOfInstruction_Value}_${this.Tag_LanguageLearned_Value}_${this.Tag_Platform_Value}_${this.Tag_Continent_Value}_${this.Tag_Country_Value}_${this.Tag_State_Value}_${this.Tag_City_Value}_${this.Tag_Feature_Value}`}
+                </label>
+            </div>
+        );
     }
 }
 
--- ProgramGuideUI/src/utils/constant.js
+++ ProgramGuideUI/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const ROLES = [{ label: 'Admin', value: 'Admin' }, { label: 'General', value: 'General' }, {label: 'Power', value: 'Power'}]
+export const ROLES = [{ label: 'Admin', value: 'Admin' }, { label: 'General', value: 'General' }, { label: 'Power', value: 'Power' }]
 export const APPLICATION_NAME = 'PROGRAMGUIDE';
 export const SEARCH_URL_HEADER = 'If you know the URL of the page, please enter below';
 export const SEARCH_TAG_HEADER = 'Please use the options below to specify one or more tags to start your search';
@@ -54,8 +54,8 @@ export const Tag_State = 'Tag_State';
 export const Tag_City = 'Tag_City';
 export const Tag_Feature = 'Tag_Feature';
 
-export const Tag_Collection = [Tag_Topic, Tag_When, Tag_CourseType, Tag_AgeRange, Tag_Duration, Tag_Duration_Details, 
-    Tag_LanguageOfInstruction, Tag_LanguageLearned,Tag_Platform, Tag_Continent,Tag_Country, Tag_State, Tag_City, Tag_Feature]
+export const Tag_Collection = [Tag_Topic, Tag_When, Tag_CourseType, Tag_AgeRange, Tag_Duration, Tag_Duration_Details,
+    Tag_LanguageOfInstruction, Tag_LanguageLearned, Tag_Platform, Tag_Continent, Tag_Country, Tag_State, Tag_City, Tag_Feature]
 
 
 
@@ -82,3 +82,10 @@ export const xmlLastmodOpen = '<lastmod>';
 export const xmlLastmodClose = '</lastmod>';
 export const xmlPriorityOpen = '<priority>'
 export const xmlPriorityClose = '</priority>'
+
+
+export const Age_Range = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+    61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+    81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99];
